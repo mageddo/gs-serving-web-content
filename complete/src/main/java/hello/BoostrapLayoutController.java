af59cffa508ced22dcb549f4e6947260da953ca6
Elvis de Freitas Souza <tqi_esouza@uolinc.com>
2016-12-13 20:45:01 -0200

    adding thymeleaf layout template example

---
+++ complete/src/main/java/hello/BoostrapLayoutController.java
@@ -0,0 +1,29 @@
+package hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author elvis
+ * @version $Revision: $<br/>
+ *          $Id: $
+ * @since 12/13/16 8:35 PM
+ */
+@Controller
+public class BoostrapLayoutController {
+
+    @RequestMapping(value ="/boostrap-layout/{user}", method = RequestMethod.GET)
+    public String bootstrapLayoutMain(
+        final @PathVariable(name = "user", required = false) String userName,
+        final Model model){
+
+        model.addAttribute("name", userName);
+        model.addAttribute("view", "layout/main-page");
+        return "layout/boostrap-layout";
+
+    }
+
+}
